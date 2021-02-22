b3e894399338b7c40e2ebaa4dd491d2bd7803747
Daniel <t.dung.lhp@gmail.com>
2021-02-21 20:39:54 -0600

    Search bar for existing result

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 import markdown2
 from markdown2 import Markdown
 from . import util
-
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -10,6 +11,7 @@ def index(request):
     })
 
 def title(request, title):
+    
     if util.get_entry(title) == None:
         return render(request, "encyclopedia/error.html")
     else:
@@ -17,4 +19,13 @@ def title(request, title):
         return render(request, "encyclopedia/title.html",{
             "content": markdowner.convert(util.get_entry(title)), "title":title
         })
+    
+    
+def search(request):
+    all_entries = util.list_entries()
+    if request.method == "GET":
+        search_entry = request.GET.get("q").upper()
+        if util.get_entry(search_entry) is not None:
+            return HttpResponseRedirect(reverse("encyclopedia:title", kwargs={"title":search_entry}))
+
 
