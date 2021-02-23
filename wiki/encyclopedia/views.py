faa2ea133a8cbcc72ac8633d0c439b53c2349dce
Daniel <t.dung.lhp@gmail.com>
2021-02-23 12:05:25 -0600

    Fixed Search and New_page complete

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django import forms
 import markdown2
 from markdown2 import Markdown
 from . import util
+
+class NewPage(forms.Form):
+    title = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
+    content = forms.CharField(widget=forms.Textarea(attrs={"class": "form-control", "rows":"3", "columns":"6"}))
+
+# All entries
+
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -12,32 +21,53 @@ def index(request):
 
 def title(request, title):
     if util.get_entry(title) == None:
-        return render(request, "encyclopedia/error.html")
+        return render(request, "encyclopedia/error.html",{
+            "search": True, 
+        })
     else:
-        markdowner = Markdown()
         return render(request, "encyclopedia/title.html",{
-            "content": markdowner.convert(util.get_entry(title)), "title":title
+            "content": Markdown().convert(util.get_entry(title)), "title":title
         })
     
     
 def search(request):
-    all_entries = util.list_entries()
     if request.method == "GET":
         search_entry = request.GET.get("q").upper()
         if util.get_entry(search_entry) is not None: #able to find result
             return HttpResponseRedirect(reverse("encyclopedia:title", kwargs={"title":search_entry}))
         else:
-            print(search_entry) 
             # there is no result
             # find the substring!
-            match = [] #this string is to store the match results
-            for entry in all_entries:
+            match = [] #this list stores the match results
+            for entry in util.list_entries():
                 if search_entry.lower() in entry.lower():
                     #return to result page
                     match.append(entry)
-
-            return render(request, "encyclopedia/result.html")
+            
+            if len(match) != 0:
+                return render(request, "encyclopedia/result.html", {
+                    "results": match 
+                })
+            else:
+                return render(request, "encyclopedia/error.html",{
+                    "search": True,
+                })
 
 
 def new_page(request):
-    return render(request, "encyclopedia/new_page.html")
+    if request.method == "POST":
+        form = NewPage(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            if title not in util.list_entries():
+                content = form.cleaned_data["content"]
+                util.save_entry(title, content)
+                return HttpResponseRedirect(reverse("encyclopedia:title", kwargs={'title': title}))
+
+            else:
+                render(request, "encyclopedia/error.html", {
+                    "search": False,
+                })
+    return render(request, "encyclopedia/new_page.html",{
+        "form": NewPage()
+    })
